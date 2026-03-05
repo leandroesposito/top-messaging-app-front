33a17ab73b263c42aa48658adc7f444ac4106ebc
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-03-05 14:34:25 -0300

    Add session buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,32 @@
+import { getUsername, isLogedIn } from "../../session/sessionManager";
 import "./Header.css";
 
-export default function Header() {
+export default function Header({ onSignUpClick, onLogInClick }) {
   return (
-    <header className="header">
+    <header className={`header ${isLogedIn() == true && "small"}`}>
       <div className="left">
         <h1>The Chat</h1>
       </div>
       <div className="right">
-        <div className="buttons"></div>
+        <div className="buttons">
+          {isLogedIn() ? (
+            <>
+              <button className="username-button" disabled>
+                {getUsername()}
+              </button>
+              <button className="log-out-button">Log out</button>
+            </>
+          ) : (
+            <>
+              <button className="sign-up-button" onClick={onSignUpClick}>
+                Sign up
+              </button>
+              <button className="login-button" onClick={onLogInClick}>
+                Log in
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </header>
   );
