b0e6f3aa423fb1c72bb6ad344ebc392222788cd1
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-03-22 16:08:23 -0300

    Remove errors from makerequest dependency array
    Every time errors where changed, makerequest was changed
    and run again on effects that use it

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -12,68 +12,62 @@ function useFetch() {
   const [data, setData] = useState(null);
   const [errors, setErrors] = useState([]);
 
-  const makeRequest = useCallback(
-    async function makeRequest(
-      route,
-      method = "GET",
-      authenticate = false,
-      body = null,
-    ) {
-      setLoading(true);
-      const options = {
-        method,
-        mode: "cors",
-        headers: { "Content-Type": "application/json" },
-      };
+  const makeRequest = useCallback(async function makeRequest(
+    route,
+    method = "GET",
+    authenticate = false,
+    body = null,
+  ) {
+    setLoading(true);
+    const options = {
+      method,
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    };
 
-      if (body) {
-        options.body = JSON.stringify(body);
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+
+    if (authenticate) {
+      options.headers.Authorization = "Bearer " + getAccessToken();
+    }
+
+    try {
+      const endpoint = domain + route;
+      const response = await fetch(endpoint, options);
+
+      if (response.status == 404) {
+        setLoading(false);
+        return ["Endpoint not found"];
       }
 
-      if (authenticate) {
-        options.headers.Authorization = "Bearer " + getAccessToken();
+      if (response.status == 500) {
+        setLoading(false);
+        return ["Server error"];
       }
+      const json = await response.json();
 
-      try {
-        const endpoint = domain + route;
-        const response = await fetch(endpoint, options);
-
-        if (response.status == 404) {
-          setLoading(false);
-          return ["Endpoint not found"];
-        }
-
-        if (response.status == 500) {
-          setLoading(false);
-          return ["Server error"];
-        }
-        const json = await response.json();
-
-        if (response.status == 401) {
-          if (
-            json.errors &&
-            json.errors[0].startsWith("Access token expired")
-          ) {
-            await refreshTokens();
-            return await makeRequest(route, method, authenticate, body);
-          } else {
-            logOut();
-            window.location.reload();
-          }
-        } else if (json.errors) {
-          setErrors([...errors, json.errors]);
+      if (response.status == 401) {
+        if (json.errors && json.errors[0].startsWith("Access token expired")) {
+          await refreshTokens();
+          return await makeRequest(route, method, authenticate, body);
         } else {
-          setData(json);
+          logOut();
+          window.location.reload();
         }
-        return setLoading(false);
-      } catch (error) {
-        console.error("FETCH ERROR", error);
-        setErrors([...errors, error.message]);
-        return setLoading(false);
+      } else if (json.errors) {
+        setErrors((errors) => [...errors, ...json.errors]);
+      } else {
+        setData(json);
       }
-    },
-    [errors],
-  );
+      return setLoading(false);
+    } catch (error) {
+      console.error("FETCH ERROR", error);
+      setErrors((errors) => [...errors, error.message]);
+      return setLoading(false);
+    }
+  }, []);
 
   return [loading, data, errors, makeRequest];
 }
