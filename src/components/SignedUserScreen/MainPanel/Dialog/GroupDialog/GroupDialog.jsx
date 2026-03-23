4ae8c3c618fb6a5fca2945d3f61f39f017b355df
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-03-22 21:56:21 -0300

    Create group dialog component

---
+++ src/components/SignedUserScreen/MainPanel/Dialog/GroupDialog/GroupDialog.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import useFetch from "../../../../../hooks/useFetch";
+import FlashMessage from "../../../../FlashMessage/FlashMessage";
+import Loading from "../../../../Loading/Loading";
+import MembersList from "./MembersList";
+import "./GroupDialog.css";
+import { getUserId } from "../../../../../session/sessionManager";
+
+export default function GroupDialog({ id, onChatClick }) {
+  const [loading, data, errors, makeRequest] = useFetch();
+  const [showCopied, setShowCopied] = useState(false);
+
+  useEffect(() => {
+    makeRequest(`/groups/${id}`, "GET", true);
+  }, [makeRequest, id]);
+
+  useEffect(() => {
+    let timeoutId;
+    if (showCopied) {
+      timeoutId = setTimeout(() => {
+        setShowCopied(false);
+      }, 2000);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showCopied]);
+
+  function onCopyClick() {
+    navigator.clipboard.writeText(data.group.inviteCode);
+    setShowCopied(true);
+  }
+
+  function onLeaveClick() {
+    makeRequest(`/groups/${id}/leave`, "DELETE", true);
+  }
+
+  function onDeleteGroupClick() {
+    makeRequest(`/groups/${id}/`, "DELETE", true);
+  }
+
+  return (
+    <div className="dialog-content">
+      {loading && <Loading />}
+      {data !== null && typeof data.group !== "undefined" && (
+        <>
+          <div className="group-data">
+            <h2>{data.group.name}</h2>
+            <p>{data.group.description}</p>
+            <div className="invite-container">
+              <span>Invite code:</span> {data.group.inviteCode}{" "}
+              <button className="flat" onClick={onCopyClick}>
+                Copy
+              </button>
+            </div>
+            {showCopied && <FlashMessage message={"Copied"} type={"success"} />}
+            <div className="buttons">
+              {data.group.ownerId === getUserId() ? (
+                <button className="danger" onClick={onDeleteGroupClick}>
+                  Delete Group
+                </button>
+              ) : (
+                <button className="danger" onClick={onLeaveClick}>
+                  Leave Group
+                </button>
+              )}
+            </div>
+          </div>
+          <MembersList
+            id={data.group.id}
+            ownerId={data.group.ownerId}
+            onChatClick={onChatClick}
+          />
+        </>
+      )}
+      {data !== null && typeof data.message !== "undefined" && (
+        <FlashMessage message={data.message} type={"success"} />
+      )}
+      {errors.map((error, index) => {
+        return <FlashMessage message={error} type={"error"} key={index} />;
+      })}
+    </div>
+  );
+}
