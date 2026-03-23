53f1df76a92c090f72913732c2360a3546dd6697
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-03-22 21:46:34 -0300

    Create my profile dialog component

---
+++ src/components/SignedUserScreen/MainPanel/Dialog/MyProfileDialog.jsx
@@ -0,0 +1,11 @@
+import PasswordForm from "./PasswordDialog";
+import ProfileForm from "./ProfileForm";
+
+export default function MyProfileDialog() {
+  return (
+    <>
+      <ProfileForm />
+      <PasswordForm />
+    </>
+  );
+}
