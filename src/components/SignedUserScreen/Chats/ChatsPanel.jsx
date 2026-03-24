bf4bddbf25613708266c487c6dc0139c8df3c1f1
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-03-24 19:47:57 -0300

    Add display/hide chats list button

--- src/components/SignedUserScreen/Chats/ChatsPanel.jsx
+++ src/components/SignedUserScreen/Chats/ChatsPanel.jsx
@@ -1,11 +1,25 @@
 import PrivateChats from "./PrivateChats";
 import GroupChats from "./GroupChats";
+import { useState } from "react";
 
 export default function ChatsPanel({ onChatClick, currentChat }) {
+  const [hidden, setHidden] = useState(false);
+
+  function onVisibilityClick() {
+    setHidden(!hidden);
+  }
+
   return (
-    <div className="chats-panel">
+    <div className={`chats-panel ${hidden ? "hidden" : ""}`}>
       <PrivateChats onChatClick={onChatClick} currentChat={currentChat} />
       <GroupChats onChatClick={onChatClick} currentChat={currentChat} />
+      <button
+        className="chats-visibility-button"
+        onClick={onVisibilityClick}
+        aria-label={`${hidden ? "Display" : "Hide"} chats list`}
+      >
+        {hidden ? ">" : "<"}
+      </button>
     </div>
   );
 }
